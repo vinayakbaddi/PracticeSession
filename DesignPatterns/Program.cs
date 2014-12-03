b7fc73729cfa4a7609d31085e09e6200c54d6a8e
vinayakbaddi <vinayakbaddi@gmail.com>
2014-12-03 12:11:40 +0530

    commit

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class Program
+    {
+        public static void main() 
+        {
+            System.Console.Write("this is me");
+        }
+    }
+}
