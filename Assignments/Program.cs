b890f1ed23c6647db3ebc191c51453d78a842afa
vinayakbaddi <vinayakbaddi@gmail.com>
2014-12-24 16:41:21 +0530

    Corrected Exception and Added TestData

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -12,7 +12,7 @@ namespace Assignments
         {
             SchedulePresenter schedulePresenter = new SchedulePresenter();
 
-            string data = "Arrange meeting for 60min\nThis is me 60min";
+            string data = "Writing Fast Tests Against Enterprise Rails 100min\r\nOverdoing it in Python lightning\r\nLua for the Masses 90min\r\nRuby Errors from Mismatched Gem Versions 45min\r\nRuby on Rails: Why We Should Move On 60min\r\nCommon Ruby Errors 45min\r\nPair Programming vs Noise 45min\r\nProgramming in the Boondocks of Seattle 30min\r\nRuby vs. Clojure for Back-End Development 30min\r\nUser Interface CSS in Rails Apps 30min";
 
             var results = schedulePresenter.SubmitSchedule(data);
             Console.WriteLine(results);
