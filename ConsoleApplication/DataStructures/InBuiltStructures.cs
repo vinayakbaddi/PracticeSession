74d7ea0c5b9473348c33b2af38c5919f0dff6bbe
vinayakbaddi <vinayakbaddi@gmail.com>
2014-12-28 18:51:22 +0530

    Data Structure

---
+++ ConsoleApplication/DataStructures/InBuiltStructures.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApplication.DataStructures
+{
+    class InBuiltStructures
+    {
+        private System.Collections.Queue ticketQueue;
+        private System.Collections.Stack functionStack;
+        private int[] intArray;
+        private System.Collections.ArrayList myArrayList; // Stores object of any type
+        private System.Collections.Hashtable myHashTable;
+        
+        private System.Collections.Generic.Dictionary<int,InBuiltStructures> myDictionary;
+        private System.Collections.Generic.IList<int> myArrayOfStronglyTypedObjects;
+        private System.Collections.Generic.LinkedList<int> myLinkedList;
+    }
+}
