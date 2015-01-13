94c01ae58cfef3f2a578189fa067528249745954
vinayakbaddi <vinayakbaddi@gmail.com>
2015-01-13 11:58:21 +0530

    Upatign Assignment and Delegat Changes

---
+++ Assignments/Services/ITalkService.cs
@@ -0,0 +1,30 @@
+﻿using Assignments.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignments.Services
+{
+    interface ITalkService
+    {
+        /// <summary>
+        /// Resets all the talks to Zero
+        /// </summary>
+        void Reset();
+
+        /// <summary>
+        /// Get the list of Talks
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Talk> GetTalks();
+
+        /// <summary>
+        /// Method to AddTalk
+        /// </summary>
+        /// <param name="rawTalksData"></param>
+        /// <returns></returns>
+        bool IsAddTalk(string rawTalksData);
+
+    }
+}
