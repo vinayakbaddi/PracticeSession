0b079914a7164a39b71d94f64441d77e3cc68203
vinayakbaddi <vinayakbaddi@gmail.com>
2017-06-02 09:51:56 +0530

    Try Catch and Finally with Environment.Exit

---
+++ ConsoleApplication/ExceptionHandling/TheTryCatchExit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication.ExceptionHandling
+{
+    public class TheTryCatchExit
+    {
+
+        public static void Main()
+        {
+            try
+            {
+                System.Console.WriteLine("HERE at Try start");
+                Environment.Exit(Environment.ExitCode);
+                System.Console.WriteLine("HERE at Try End");
+                System.Console.ReadLine();
+
+            }
+            catch (Exception ex) { }
+            finally
+            {
+                System.Console.WriteLine("HERE at FInally");
+                System.Console.ReadLine();
+
+
+            }
+        }
+    }
+}
