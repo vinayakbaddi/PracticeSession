a64304f7a74df7fcf428131013f4e16c15e49e2d
Vinayak Baddi <vinayakbaddi@gmail.com>
2023-01-29 18:55:46 +0530

    added XML xpath

---
+++ ConsoleApplication/XML/XmlManipulation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace ConsoleApplication.XML
+{
+    public class XmlManipulation
+    {
+        void t()
+        {
+            // Create an XmlDocument object and load the XML
+            XmlDocument doc = new XmlDocument();
+            doc.Load("example.xml");
+
+            // Create an XPathNavigator object to navigate the XML
+            XPathNavigator nav = doc.CreateNavigator();
+
+            // Define the namespace prefix and URI
+            XmlNamespaceManager ns = new XmlNamespaceManager(nav.NameTable);
+            ns.AddNamespace("ns", "http://example.com/namespace");
+
+            // Use the XPathNavigator to select the elements we want
+            XPathNodeIterator nodes = nav.Select("//ns:elementName", ns);
+
+            // Iterate through the selected elements
+            while (nodes.MoveNext())
+            {
+                Console.WriteLine(nodes.Current.Value);
+            }
+        }
+    }
+}
